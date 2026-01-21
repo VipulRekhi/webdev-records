e77e7eb1c882d76fb14c10ec1c07c2686b16d5f2
VipulRekhi <vipulrekhi@gmail.com>
2026-01-21 17:37:14 +0530

    sound to switch

--- javaScript/Drum Kit Starting Files/index.js
+++ javaScript/Drum Kit Starting Files/index.js
@@ -2,7 +2,21 @@ for(var i=0;i<7;i++)
 {
     document.querySelectorAll("button")[i].addEventListener("click",function()
 {
-    var audio = new Audio('./sounds/crash.mp3');
+   var letter=this.innerHTML;
+   switch(this.innerHTML)
+   {
+   case "w":
+    var audio = new Audio("./sounds/crash.mp3");
     audio.play();
-})
+    break;
+    case "a":
+    var audio = new Audio("./sounds/kick-bass.mp3");
+    audio.play();
+    break;
+    case "s":
+    var audio = new Audio("./sounds/crash.mp3");
+    audio.play();
+    break;
+   }
 }
+);}
