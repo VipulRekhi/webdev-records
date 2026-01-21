52222dfebbb70aa108cdbff2930252c55037fc39
VipulRekhi <vipulrekhi@gmail.com>
2026-01-21 17:11:18 +0530

    first a

--- javaScript/Drum Kit Starting Files/index.js
+++ javaScript/Drum Kit Starting Files/index.js
@@ -2,6 +2,7 @@ for(var i=0;i<7;i++)
 {
     document.querySelectorAll("button")[i].addEventListener("click",function()
 {
-    alert("hehe i got clicked");
+    var audio = new Audio('./sounds/crash.mp3');
+    audio.play();
 })
 }
