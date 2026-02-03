3565ae50429a4ecea7939af71928c405a18ac2e3
VipulRekhi <vipulrekhi@gmail.com>
2026-02-03 18:12:45 +0530

    3 pages on /

--- BackEnd/express using node/first server/index.js
+++ BackEnd/express using node/first server/index.js
@@ -4,6 +4,14 @@ app.get("/",(req,res) =>
 {
     res.send("<h1>hello world<h1>");
 });
+app.get("/about",(req,res) =>
+{
+    res.send("<h1>hello this is about me<h1>");
+});
+app.get("/contact",(req,res) =>
+{
+    res.send("<h1>hello this is my contact number<h1>");
+});
 app.listen(3000,()=>
 {
     console.log("server running on port 3000");
