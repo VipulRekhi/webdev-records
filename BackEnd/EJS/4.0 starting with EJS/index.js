e2c819b00422eda0a75f2b34d3b3683e069f8458
VipulRekhi <vipulrekhi@gmail.com>
2026-02-05 18:50:43 +0530

    weekday

--- BackEnd/EJS/4.0 starting with EJS/index.js
+++ BackEnd/EJS/4.0 starting with EJS/index.js
@@ -1,2 +1,27 @@
 import express from "express";
-import ejs from "ejs";
+const app =express();
+const port =3000;
+
+app.get("/",(req,res)=>
+{
+    const d =new Date();
+    let day =d.getDay()
+    console.log(day);
+    let weekday="Weekday";
+    let advice="hey its time to grind";
+    if(day ===0 || day ===6)
+    {
+        let weekday="WeekEnd";
+        let advice="hey its time to have Fun";
+
+    }
+    res.render("index.ejs",{
+        dayselector:weekday,
+        adv:advice
+    });
+});
+
+app.listen(port , ()=>
+{
+    console.log(`you are listening to ${port}`);
+});
