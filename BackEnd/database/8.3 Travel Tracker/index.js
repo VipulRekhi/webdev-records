c2429c111c1faebd493ded218e8ec67c52be962b
VipulRekhi <vipulrekhi@gmail.com>
2026-06-06 17:17:54 +0530

    t2 done

--- BackEnd/database/8.3 Travel Tracker/index.js
+++ BackEnd/database/8.3 Travel Tracker/index.js
@@ -17,6 +17,7 @@ const db=new pg.Client(
 db.connect();
 
 
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
@@ -29,13 +30,26 @@ app.get("/", async (req, res) => {
   countrylist.push(country.country_code);
   }
 );
-  db.end();
+  
   res.render("index.ejs",
   {
     countries:countrylist,
     total:countrylist.length
   });
 });
+
+app.post("/add", async (req,res)=>
+{
+  const new_country = req.body.country;
+  const result= await db.query("select country_code from countries where country_name = $1",[new_country]);
+  const data = result.rows[0].country_code;
+  console.log(data);
+  await db.query("INSERT INTO visited_countries (country_code) VALUES ($1)", [
+      data
+    ]);
+  res.redirect("/");
+});
+
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
