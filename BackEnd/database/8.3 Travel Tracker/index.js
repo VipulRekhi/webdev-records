a8be946cee69b816bca242b9c1092ccf1e676d10
VipulRekhi <vipulrekhi@gmail.com>
2026-06-06 16:45:30 +0530

    t1 done

--- BackEnd/database/8.3 Travel Tracker/index.js
+++ BackEnd/database/8.3 Travel Tracker/index.js
@@ -1,14 +1,40 @@
 import express from "express";
+import pg from "pg";
 import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
+const db=new pg.Client(
+  {
+    user:"postgres",
+    host:"localhost",
+    database:"world",
+    password:"worldpass",
+    port:5432
+  }
+);
+
+db.connect();
+
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", async (req, res) => {
   //Write your code here.
+  const result = await db.query("select country_code from visited_countries");
+  let countrylist=[];
+  result.rows.forEach((country)=>
+  {
+  countrylist.push(country.country_code);
+  }
+);
+  db.end();
+  res.render("index.ejs",
+  {
+    countries:countrylist,
+    total:countrylist.length
+  });
 });
 
 app.listen(port, () => {
